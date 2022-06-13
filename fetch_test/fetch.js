d92bc43fb8a89387672d3faa57f0989a259983b7
RyoyaYamada <r.yamada@unirobot.com>
2022-06-13 21:53:50 +0900

    nodejsの練習

---
+++ fetch_test/fetch.js
@@ -0,0 +1,25 @@
+
+async function test(){
+    const response = await fetch("http://localhost:3000",{
+        method:'GET',
+        mode:'no-cors',
+        credentials:'include',
+        cache:'default',
+        headers:{
+            'Content-Type':'application/json'
+        }
+    });
+
+    console.log(response.status);
+
+    if(response.ok){
+        const json = await response.json();
+    console.log(json);
+    }
+}
+
+test()
+
+
+
+
